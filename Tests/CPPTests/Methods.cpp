35308ddda4431ec853a36e06e5924adb991a0f62
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-05-06 21:04:11 +1000

    [Tests] Add some return value tests.

--- Tests/CPPTests/Methods.cpp
+++ Tests/CPPTests/Methods.cpp
@@ -4,12 +4,14 @@
 #include <fstream>
 #include <string>
 
-TEST_CASE("Static Void Method") {
+TEST_CASE("CSharpTests/Methods/TestStaticMethod writes expected output to file") {
+    const char* filePath = "STATICVOIDMETHODSTEST.txt";
+
     // Call the C# static method which writes to test.txt
     NetLeaf::RunCSharpMethod("CSharpTests.Methods.TestStaticMethod()");
 
     // Read the output from the file
-    std::ifstream file("STATICVOIDMETHODSTEST.txt");
+    std::ifstream file(filePath);
     REQUIRE(file.is_open());
 
     std::string fileContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
@@ -19,5 +21,20 @@ TEST_CASE("Static Void Method") {
     REQUIRE(fileContent.find("TestStaticMethod() Ran successfully!") != std::string::npos);
 
     // Delete the file
-	std::remove("STATICVOIDMETHODSTEST.txt");
+	std::remove(filePath);
+}
+
+TEST_CASE("CSharpTests/Methods/TestStaticIntMethod Returns expected integer value") {
+	MethodReturnValue result = NetLeaf::RunCSharpMethod("CSharpTests.Methods.TestStaticIntMethod()");
+	REQUIRE(result.IntResult == 32);
+}
+
+TEST_CASE("CSharpTests/Methods/TestStaticStringMethod Returns expected string value") {
+	MethodReturnValue result = NetLeaf::RunCSharpMethod("CSharpTests.Methods.TestStaticStringMethod()");
+	REQUIRE(result.StringResult == "ABC123");
+}
+
+TEST_CASE("CSharpTests/Methods/TestStaticUIntMethod Returns expected uint value") {
+	MethodReturnValue result = NetLeaf::RunCSharpMethod("CSharpTests.Methods.TestStaticUIntMethod()");
+	REQUIRE(result.UIntResult == 32);
 }
--- Tests/CSharpTests/Methods.cs
+++ Tests/CSharpTests/Methods.cs
@@ -7,5 +7,20 @@
             // Write to a file
             File.WriteAllText("STATICVOIDMETHODSTEST.txt", "TestStaticMethod() Ran successfully!");
         }
+
+        public static int TestStaticIntMethod()
+        {
+            return 32;
+        }
+
+        public static uint TestStaticUIntMethod()
+        {
+            return 32;
+        }
+
+        public static string TestStaticStringMethod()
+        {
+            return "ABC123";
+        }
     }
 }
